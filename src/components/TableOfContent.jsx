fbd964b08b00ff883e7dd5bfc486e23671f39969
Ian Thorslund <ian.thorslund@gmail.com>
2022-08-23 20:29:32 +0200

    Adding table of contents

---
+++ src/components/TableOfContent.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function TableOfContent() {
+  return (
+    <ul>
+      <li>
+        <a href="./Javascript/Object#property-identifier">
+          Property Identifier
+        </a>
+      </li>
+      <li>
+        <a href="./Javascript/Object#computed-property-names">
+          Computed Property Names
+        </a>
+      </li>
+      <li>
+        <a href="./Javascript/Object#shorthand-property-names">
+          Shorthand Property Names
+        </a>
+      </li>
+      <li>
+        <a href="./Javascript/Arrays#init">Init</a>
+      </li>
+      <li>
+        <a href="./Javascript/Arrays#Push">Push</a>
+      </li>
+    </ul>
+  );
+}
